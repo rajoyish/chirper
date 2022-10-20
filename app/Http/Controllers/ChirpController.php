aa3cd010ebfa7efb6f31b6e4ab999b35ff8d5bc0
Rajesh Budhathoki <rajoyish@gmail.com>
2022-10-20 09:11:47 +0545

    Saving the Chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -15,7 +15,6 @@ class ChirpController extends Controller
     public function index()
     {
         return view('chirps.index');
-
     }
 
     /**
@@ -36,7 +35,13 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-       
+        $validated = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
